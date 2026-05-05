e0a2a34fe0e19a307d588bf32329d0c5a6fbb8c3
Yengzzkie <gatchalian.manuel@gmail.com>
2026-05-05 15:30:15 -0400

    added carousel component for featured items

--- components/FeaturedMenu/FeaturedMenu.tsx
+++ components/FeaturedMenu/FeaturedMenu.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 import { Noto_Serif } from "next/font/google";
+import Carousel from "../ui/Carousel/Carousel";
 
 const notoSerif = Noto_Serif({
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -11,35 +10,35 @@ const notoSerif = Noto_Serif({
 
 const menuItems = [
   {
-    title: "Boodle Fight",
+    name: "Boodle Fight",
     category: "Boodle Fight",
     image: "/618808436_910181301948328_108844109515198654_n.jpg",
     description: "48-hour slow-cooked duck leg with cane vinegar and soy.",
     price: "CAD 28",
   },
   {
-    title: "Kakakanin Platter",
+    name: "Kakakanin Platter",
     category: "Main",
     image: "/kakanin-platter.png",
     description: "Peanut-infused risotto with beef short rib.",
     price: "CAD 32",
   },
   {
-    title: "Halo-Halo",
+    name: "Halo-Halo",
     category: "Dessert",
     image: "/halo-halo.png",
     description: "Refined Filipino dessert with ube and leche flan.",
     price: "CAD 16",
   },
   {
-    title: "Sizzling Bangus Sisig",
+    name: "Sizzling Bangus Sisig",
     category: "Sizzling",
     image: "/SIZZLING BANGUS SISIG.png",
     description: "Crispy croquettes with sisig and calamansi aioli.",
     price: "CAD 18",
   },
   {
-    title: "Sizzling Crispy Sisig",
+    name: "Sizzling Crispy Sisig",
     category: "Sizzling",
     image: "/SIZZLING CRISPY SISIG.png",
     description: "Charcoal-grilled chicken with annatto marinade.",
@@ -47,21 +46,11 @@ const menuItems = [
   },
 ];
 
-const ITEM_WIDTH = 100 / 3; // 3 items visible
 
 const FeaturedMenu = () => {
-  const [index, setIndex] = useState(0);
-
-  const handlePrev = () => {
-    setIndex((prev) => (prev === 0 ? menuItems.length - 3 : prev - 1));
-  };
-
-  const handleNext = () => {
-    setIndex((prev) => (prev >= menuItems.length - 3 ? 0 : prev + 1));
-  };
 
   return (
-    <section className="bg-(--secondary-bg) text-(--tertiary) py-32 px-16">
+    <section className="bg-(--secondary-bg) text-(--tertiary) py-16 lg:py-32 px-8 lg:px-16">
       <div className="max-w-screen-2xl mx-auto">
         {/* Header */}
         <div className="flex justify-between items-end mb-20">
@@ -74,33 +63,17 @@ const FeaturedMenu = () => {
               ancestrally rooted.
             </h2>
           </div>
-
-          <div className="flex gap-4">
-            <button
-              onClick={handlePrev}
-              className="w-12 h-12 border flex items-center justify-center hover:bg-tertiary hover:text-white"
-            >
-              <FaArrowLeft />
-            </button>
-            <button
-              onClick={handleNext}
-              className="w-12 h-12 border flex items-center justify-center hover:bg-tertiary hover:text-white"
-            >
-              <FaArrowRight />
-            </button>
-          </div>
         </div>
 
         {/* Carousel */}
-        <div className="overflow-hidden">
+        <Carousel items={menuItems} />
+        {/* <div className="overflow-hidden">
           <div
             className="flex transition-transform duration-700 ease-[cubic-bezier(0.22,1,0.36,1)]"
-            style={{
-              transform: `translateX(-${index * ITEM_WIDTH}%)`,
-            }}
+            style={{ transform: `translateX(-${index * ITEM_WIDTH}%)`}}
           >
             {menuItems.map((item, i) => (
-              <div key={i} className="w-full md:w-1/3 flex-shrink-0 px-6">
+              <div key={i} className="w-full md:w-1/3 shrink-0 px-6">
                 <div className="group">
                   <div className="aspect-square bg-stone-200 mb-8 overflow-hidden relative">
                     <img
@@ -110,9 +83,7 @@ const FeaturedMenu = () => {
                     />
                   </div>
 
-                  <h3
-                    className={`${notoSerif.className} text-2xl font-bold mb-3`}
-                  >
+                  <h3 className={`${notoSerif.className} text-2xl font-bold mb-3`}>
                     {item.title}
                   </h3>
 
@@ -125,7 +96,7 @@ const FeaturedMenu = () => {
               </div>
             ))}
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
---
+++ components/ui/Carousel/Carousel.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { useRef } from "react";
+import Slider, { Settings } from "react-slick";
+import type { CustomArrowProps } from "react-slick";
+import { GoArrowLeft, GoArrowRight } from "react-icons/go";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+type featuredItemProps = {
+  name: string;
+  category: string;
+  image: string;
+  description: string;
+  price: string;
+};
+
+type itemProps = { items: featuredItemProps[] };
+
+const SampleNextArrow = ({ onClick }: CustomArrowProps) => (
+  <button
+    onClick={onClick}
+    className="
+      hidden md:block
+      absolute right-[-3%] top-1/2 -translate-y-1/2 z-10
+      border border-[#8e3a1f] text-[#8e3a1f]
+      p-3
+      transition-all duration-300
+      hover:bg-[#8e3a1f]
+      hover:text-white
+      hover:scale-110
+    "
+  >
+    <GoArrowRight />
+  </button>
+);
+
+const SamplePrevArrow = ({ onClick }: CustomArrowProps) => (
+  <button
+    onClick={onClick}
+    className="
+      hidden md:block
+      absolute left-[-3%] top-1/2 -translate-y-1/2 z-10
+      border border-[#8e3a1f] text-[#8e3a1f]
+      p-3
+      transition-all duration-300
+      hover:bg-[#8e3a1f]
+      hover:text-white
+      hover:scale-110
+    "
+  >
+    <GoArrowLeft />
+  </button>
+);
+
+export default function Carousel({ items }: itemProps) {
+  const sliderRef = useRef<Slider | null>(null);
+
+  const settings: Settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2500,
+    cssEase: "ease-in-out",
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="slider-container px-4 md:px-8">
+      <Slider ref={sliderRef} {...settings}>
+        {items.map((item, index) => (
+          <div key={index} className="px-3">
+            <div className="group">
+              <div className="aspect-square overflow-hidden mb-6">
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
+                />
+              </div>
+
+              <h3 className="text-2xl font-bold mb-2">{item.name}</h3>
+
+              <p className="mb-3 text-sm text-gray-500">{item.description}</p>
+
+              <span className="font-semibold text-(--primary)">CAD 28</span>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
