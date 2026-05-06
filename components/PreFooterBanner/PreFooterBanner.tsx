5a36dd621c9e0d4d4901db3c3dfb92a3e616507f
Yengzzkie <gatchalian.manuel@gmail.com>
2026-05-05 21:00:38 -0400

    added links to skipthedishes and ubereats button

--- components/PreFooterBanner/PreFooterBanner.tsx
+++ components/PreFooterBanner/PreFooterBanner.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const PreFooterBanner = () => {
   return (
@@ -12,34 +13,35 @@ const PreFooterBanner = () => {
         />
       </div>
       <div className="relative z-10 max-w-4xl mx-auto text-center text-white px-8 lg:px-16">
-        <h2 className="noto-serif font-semibold text-5xl mb-8">
-          Begin your journey home.
+        <h2 className="noto-serif font-semibold text-3xl lg:text-5xl mb-4 lg:mb-8">
+          Order Online Now
         </h2>
-        <p className="text-(--neutral) mb-12 opacity-90">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-          eius consectetur eum temporibus, iusto quam laudantium in eveniet
-          quisquam ex commodi, culpa maiores autem minima sit natus, aperiam rem
-          fuga.
+        <p className="text-(--neutral) text-xs lg:text-sm mb-6 lg:mb-12 opacity-90">
+          Freshly prepared Filipino favorites—order now and enjoy fast delivery while it&apos;s hot.
         </p>
         <div className="flex flex-col md:flex-row justify-center gap-6">
-          <button className="flex flex-col items-center bg-white text-(--primary) px-12 py-5 uppercase tracking-widest border border-white hover:bg-(--primary-light) hover:border-(--primary-light) transition-all">
-            <span>Order on</span>
-            <Image
-              src="/SkipTheDishes.png"
-              alt="skipthedishes"
-              width={100}
-              height={100}
-            />
-          </button>
-          <button className="flex flex-col items-center gap-2 bg-white text-(--primary) px-12 py-5 uppercase tracking-widest border border-white hover:bg-(--primary-light) hover:border-(--primary-light) transition-all">
-            <span>Order on</span>
-            <Image
-              src="/UberEatsLogo.png"
-              alt="ubereats"
-              width={100}
-              height={100}
-            />
-          </button>
+          <Link href="https://www.skipthedishes.com/pasalubong-bond" target="_blank">
+            <button className="flex flex-col items-center bg-white text-(--primary) px-12 py-5 uppercase tracking-widest border border-white hover:bg-(--primary-light) hover:border-(--primary-light) transition-all h-24 mx-auto">
+              <span>Order on</span>
+              <Image
+                src="/SkipTheDishes.png"
+                alt="skipthedishes"
+                width={100}
+                height={100}
+              />
+            </button>
+          </Link>
+          <Link href="https://www.ubereats.com/ca/store/pasalubong-905/qrJQk32dS7GtkBSoQ8LtUQ?srsltid=AfmBOoq2qukY3zGHIt7OesEpE6ECKvgh4OKUZGsG4O5Ftxm3LV60hALk" target="_blank">
+            <button className="flex flex-col items-center gap-2 bg-white text-(--primary) px-12 py-5 uppercase tracking-widest border border-white hover:bg-(--primary-light) hover:border-(--primary-light) transition-all h-24 mx-auto">
+              <span>Order on</span>
+              <Image
+                src="/UberEatsLogo.png"
+                alt="ubereats"
+                width={100}
+                height={100}
+              />
+            </button>
+          </Link>
         </div>
       </div>
     </section>
