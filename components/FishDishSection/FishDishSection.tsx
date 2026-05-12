60bf9adc88d0d8fb325e14b00fdc6420e12d42d8
Yengzzkie <gatchalian.manuel@gmail.com>
2026-05-11 20:25:00 -0400

    added price on fish dishes and adjusted layout for pricing

--- components/FishDishSection/FishDishSection.tsx
+++ components/FishDishSection/FishDishSection.tsx
@@ -1,18 +1,26 @@
 import Image from "next/image";
-import { Noto_Serif } from "next/font/google";
 import fishDishes from "@/data/fish-dishes.json";
 
-const notoSerif = Noto_Serif({
-  weight: ["400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-});
+type fishDishProps = {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+  price: {
+    small: string;
+    medium: string;
+    large: string;
+  };
+  isSingleServe: boolean;
+  singleServePrice: string;
+};
 
 const ChickenDishSection = () => {
   return (
     <section className="mb-32 scroll-mt-50" id="seafood-dishes">
       <div className="grid grid-cols-1 md:grid-cols-12 gap-12 items-center mb-16 flex-row-reverse">
         <div className="md:col-span-5 order-2 md:order-1 text-right">
-          <h2 className={`${notoSerif.className} font-bold text-(--primary) text-2xl lg:text-5xl mb-2 lg:mb-4`}>
+          <h2 className="noto-serif font-bold text-(--primary) text-2xl lg:text-5xl mb-2 lg:mb-4">
             Fish Dishes
           </h2>
           <p className="text-xs lg:text-base">
@@ -32,7 +40,7 @@ const ChickenDishSection = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-x-20 gap-y-3">
-        {fishDishes.map((fish) => (
+        {fishDishes.map((fish: fishDishProps) => (
           <div key={fish.id} className="flex justify-between pb-6">
             <div className="flex items-center gap-4 border-b-2 border-(--primary) pb-4 max-w-[80%]">
               <div className="w-20 lg:w-36 h-20 lg:h-36 shrink-0 overflow-hidden">
@@ -45,42 +53,65 @@ const ChickenDishSection = () => {
                 />
               </div>
               <div>
-                <h3 className={`${notoSerif.className} font-semibold text-xl mb-2`}>
+                <h3 className="noto-serif font-semibold text-xl mb-2">
                   {fish.name}
                 </h3>
                 <p className="text-xs italic">{fish.description}</p>
               </div>
             </div>
-            <div className="flex flex-col gap-2 items-center mt-2 lg:mt-8">
-              <div className="flex flex-col items-center">
-                <span className="w-6 aspect-square flex items-center justify-center bg-(--primary) text-(--primary-light)">
-                  S
-                </span>
-                <span className="text-xs lg:text-base self-start text-(--primary)">
-                  ${fish.price.small}
-                </span>
-              </div>
-              <div className="flex flex-col items-center">
-                <span className="w-6 aspect-square flex items-center justify-center bg-(--primary) text-(--primary-light)">
-                  M
-                </span>
-                <span className="text-xs lg:text-base self-start text-(--primary)">
-                  ${fish.price.medium}
-                </span>
-              </div>
-              <div className="flex flex-col items-center">
-                <span className="w-6 aspect-square flex items-center justify-center bg-(--primary) text-(--primary-light)">
-                  L
-                </span>
-                <span className="text-xs lg:text-base self-start text-(--primary)">
-                  ${fish.price.large}
-                </span>
-              </div>
-            </div>
+            {fish.isSingleServe
+              ? singleServePrice(fish)
+              : multiSizeServingPrice(fish)}
           </div>
         ))}
       </div>
     </section>
+  );
+};
+
+const singleServePrice = (fish: fishDishProps) => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <span className="flex items-center justify-center bg-(--primary) text-(--primary-light) text-[8px] lg:text-sm text-center rounded-lg mb-1 px-1 lg:px-2 py-0.5 lg:py-1">
+        Single Serve
+      </span>
+      <span className="text-xs lg:text-base text-(--primary)">
+        ${fish.singleServePrice}
+      </span>
+    </div>
+  );
+};
+
+const multiSizeServingPrice = (fish: fishDishProps) => {
+  return (
+    <div className="flex flex-col gap-2 items-center mt-2 lg:mt-8">
+      {fish.price.small && (
+        <div className="flex flex-col items-center">
+          <span className="w-6 aspect-square flex items-center justify-center bg-(--primary) text-(--primary-light)">
+            S
+          </span>
+          <span className="text-xs lg:text-base self-start text-(--primary)">
+            ${fish.price.small || null}
+          </span>
+        </div>
+      )}
+      <div className="flex flex-col items-center">
+        <span className="w-6 aspect-square flex items-center justify-center bg-(--primary) text-(--primary-light)">
+          M
+        </span>
+        <span className="text-xs lg:text-base self-start text-(--primary)">
+          ${fish.price.medium || null}
+        </span>
+      </div>
+      <div className="flex flex-col items-center">
+        <span className="w-6 aspect-square flex items-center justify-center bg-(--primary) text-(--primary-light)">
+          L
+        </span>
+        <span className="text-xs lg:text-base self-start text-(--primary)">
+          ${fish.price.large || null}
+        </span>
+      </div>
+    </div>
   );
 };
 
